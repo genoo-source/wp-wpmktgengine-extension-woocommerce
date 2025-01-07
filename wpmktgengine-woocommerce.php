b4dbebe4528e3084a526ec2b330822e9804ade27
GenooSource <50386609+GenooSource@users.noreply.github.com>
2025-01-06 19:23:12 -0600

    Update wpmktgengine-woocommerce.php

--- wpmktgengine-woocommerce.php
+++ wpmktgengine-woocommerce.php
@@ -5,7 +5,7 @@ Description: Genoo, LLC
 Author:  Genoo, LLC
 Author URI: http://www.genoo.com/
 Author Email: info@genoo.com
-Version: 1.7.50
+Version: 1.7.51
 License: GPLv2
 WC requires at least: 3.0.0
 WC tested up to: 5.2.3 */
